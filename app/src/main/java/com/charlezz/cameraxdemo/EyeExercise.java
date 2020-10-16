cda61eb782d88df79130a347a80f369a73030929
xxsweetmealxx <xxsweetmealxx@gmail.com>
2020-10-16 10:47:49 +0900

    눈운동 UI

---
+++ app/src/main/java/com/charlezz/cameraxdemo/EyeExercise.java
@@ -0,0 +1,14 @@
+package com.charlezz.cameraxdemo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EyeExercise extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.eye_exercise);
+    }
+}
