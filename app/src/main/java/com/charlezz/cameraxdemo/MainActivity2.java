c8dc39399854d4b917df7c798fa0f30fd771af4b
tofha1107 <romzoa@naver.com>
2020-10-19 16:26:13 +0900

    새롬 10.19 15:09 카운트다운 시도1 + 회원가입 창 완료 + 로그인 창 완료

--- app/src/main/java/com/charlezz/cameraxdemo/MainActivity2.java
+++ app/src/main/java/com/charlezz/cameraxdemo/MainActivity2.java
@@ -3,12 +3,18 @@ package com.charlezz.cameraxdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
     }
-}
+
+    }
