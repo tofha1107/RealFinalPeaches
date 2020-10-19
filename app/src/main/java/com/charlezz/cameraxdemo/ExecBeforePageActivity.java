170a058375675f13e2b05d849a1ad39c6e9f4e77
tofha1107 <romzoa@naver.com>
2020-10-19 10:34:46 +0900

    새롬 10.19 10:34

---
+++ app/src/main/java/com/charlezz/cameraxdemo/ExecBeforePageActivity.java
@@ -0,0 +1,14 @@
+package com.charlezz.cameraxdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExecBeforePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.exec_before_page);
+    }
+}
