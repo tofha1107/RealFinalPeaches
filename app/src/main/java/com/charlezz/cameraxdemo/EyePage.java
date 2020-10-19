c2c6fa6583b9d9d52975997bd62920c3f9e19e4d
xxsweetmealxx <xxsweetmealxx@gmail.com>
2020-10-19 12:10:21 +0900

    거리설정, 시간설정page

---
+++ app/src/main/java/com/charlezz/cameraxdemo/EyePage.java
@@ -0,0 +1,14 @@
+package com.charlezz.cameraxdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EyePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.eye_page);
+    }
+}
